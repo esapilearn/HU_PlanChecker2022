9084568e33d0787032e50ba197db8b1ca435236a
Yibing Hu <huyibing@yahoo.com>
2022-03-16 10:48:43 -0700

    Unit test need more work.

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void CTDateFailCorrectly()
+        {
+            //Arange
+            var ctDate = DateTime.Now.AddDays(-61);
+
+            // Act
+            var actual = new CTDateConstraint().ConstraintCTDate(ctDate).ResultType;
+
+            //Assert
+            
+            Assert.AreEqual(expected, actual);
+        }
+        public void CTDatePassCorrectly()
+        {
+            //Arange
+
+
+
+
+
+            Assert.Fail();
+        }
+    }
+}
